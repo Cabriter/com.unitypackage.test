c66c8cd104194d38cbc304179a37debaa5a4f2f8
cabriter <dreamcabrite@gmail.com>
2021-08-22 00:52:21 +0800

    init

---
+++ Editor/LoggerEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using FW.Logger;
+
+namespace FW.Logger
+{
+    public class LoggerEditor : Editor
+    {
+        [MenuItem("Logger/Log")]
+        public static void Log()
+        {
+            Debug.Log("aaa");
+        }
+    }
+}
---
+++ Runtime/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FW.Logger
+{
+    public static class Logger
+    {
+        public static void Log(string log)
+        {
+            Debug.Log(log);
+        }
+    }
+}
+
